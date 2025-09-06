73513e5bd8a365c35ba1a34fd020e7a24e1e299f
Santi-I-Guess <aquaeagle10@gmail.com>
2025-09-06 14:53:37 -0600

    smth

--- problem_e.py
+++ problem_e.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+from itertools import accumulate
+
 '''
     PROBLEM E:
 
@@ -7,11 +10,32 @@
         camps_by_height = sort camps on camp height
         return camps_by_height[0], camps_by_height[-1]
 '''
+
+@dataclass
+class campsite:
+    camp_num: int
+    height: int
+
 if __name__ == '__main__':
     test_cases = int(input())
     for test_case_num in range(test_cases):
-        input()
-        altitudes = input().split(" ")
-        print(*altitudes)
-        campsite.height = sum(altitudes[:campsite_num-1])
-
+        input() # unneeded
+        altitude_changes = [int(i) for i in input().split(" ")]
+        altitudes = list(accumulate(altitude_changes))
+        the_min = 0
+        the_max = 0
+        the_min_idx = 1
+        the_max_idx = 1
+        altitudes = [0] + altitudes
+        for idx, i in enumerate(altitudes):
+            if idx == 0:
+                the_min = i
+                the_max = i
+                continue
+            if i < the_min:
+                the_min = i
+                the_min_idx = idx + 1
+            if i > the_max:
+                the_max = i
+                the_max_idx = idx + 1
+        print(f"{the_min_idx} {the_max_idx}")
