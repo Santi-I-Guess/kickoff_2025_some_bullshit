2b1f1b46b001bf75e4760a71f98d145649a5ac4c
Santi-I-Guess <aquaeagle10@gmail.com>
2025-09-06 16:33:41 -0600

    smsmsmsmsm

--- problem_g.py
+++ problem_g.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
         towns_rev.update({int(mile_marker):town_name})
     for i in range(num_questions):
         question_type, question_input = input().split()
-        if question_type == "1":
+        if   question_type == "1":
             print(towns_rev[int(question_input)])
         elif question_type == "2":
             print(towns[question_input])
